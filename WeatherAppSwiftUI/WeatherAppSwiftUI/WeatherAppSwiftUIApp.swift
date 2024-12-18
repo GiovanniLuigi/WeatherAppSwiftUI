6b21367ef8efff9dff0898486238469ffab3fe57
Giovanni Bruno <giovannibruno128@gmail.com>
2024-12-18 09:58:59 -0300

    Project creation

---
+++ WeatherAppSwiftUI/WeatherAppSwiftUI/WeatherAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUIApp.swift
+//  WeatherAppSwiftUI
+//
+//  Created by Giovanni Bruno on 17/12/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherAppSwiftUI/WeatherAppSwiftUITests/WeatherAppSwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppSwiftUITests.swift
+//  WeatherAppSwiftUITests
+//
+//  Created by Giovanni Bruno on 17/12/24.
+//
+
+import Testing
+@testable import WeatherAppSwiftUI
+
+struct WeatherAppSwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
